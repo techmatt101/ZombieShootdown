b9e87eecc79262ac2084df1d4fcbbfea488ab2a6
techmatt101 <matt@kempmail.co.uk>
2014-11-06 13:23:26 +0000

    linux bug fixes

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
-py build_references.py
+python build_references.py
 tsc src/app.ts --out src/build/app.js --sourcemap --target ES5
--- build_references.py
+++ build_references.py
@@ -1,17 +1,22 @@
 import os
+import sys
 
 files = []
 		 		 
 def add(folder):
-	files.extend(''.join('/// <reference path="' + root.replace('src\\', '') + '\\' + file + '" />')
+	files.extend(''.join('/// <reference path="' + root.replace('src/', '') + '/' + file + '" />')
 			for root, dirs, files in os.walk(folder)
 			for file in files
 			if file.endswith('.ts')
 			if not '_' in file
 		)
-			 
-add('src\\lib')
-add('src\\core')
-text_file = open("src\\ref.ts", "w")
+
+srcPath = sys.path[0]
+add('src/lib')
+add('src/core')
+
+text_file = open("src/ref.ts", "w")
 text_file.write("\n".join(files))
 text_file.close()
+
+print "References Updated"
