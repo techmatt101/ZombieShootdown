0875b2e63ebd4c4bb0afa86867c01161f212ad04
techmatt101 <matt@kempmail.co.uk>
2014-11-12 03:53:18 +0000

    performance  tweak

--- src/core/systems/CollisionSystem.ts
+++ src/core/systems/CollisionSystem.ts
@@ -11,7 +11,7 @@ class CollisionSystem implements ISystem {
     update (dt : number) {
         for (var i = 0; i < this._entities.length; i++) {
             var collision = <Collision> (<any>this._entities[i].components).Collision; //TODO: hmmm...
-            for (var ii = 0; ii < this._entities.length; ii++) {
+            for (var ii = i; ii < this._entities.length; ii++) {
                 if (this._entities[i] !== this._entities[ii]) {
                     collision.test( <Collision> (<any>this._entities[ii].components).Collision); //TODO: hmmm...
                 }
