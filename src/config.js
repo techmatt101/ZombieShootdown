b43c80d048eb59b58a0162e464e4d6601e3ebcd4
techmatt101 <matt@kempmail.co.uk>
2014-11-24 23:37:52 +0000

    more key mappings

--- src/config.js
+++ src/config.js
@@ -4,11 +4,15 @@ var Config = {
     skipMenu: true,
 
     keyMappings: {
-        32: 'UP', //Space Key
         87: 'UP', //W Key
-        83: 'DOWN', //S Key
-        65: 'LEFT', //A Key
         68: 'RIGHT', //D Key
+        65: 'LEFT', //A Key
+        83: 'DOWN', //S Key
+        38: 'UP', //UP Key
+        40: 'DOWN', //DOWN Key
+        37: 'LEFT', //LEFT Key
+        39: 'RIGHT', //RIGHT Key
+        32: 'ACTION_1', //Space Key
         0: 'ACTION_1', //Left Click
         2: 'ACTION_2', //Right Click
         27: 'MENU' //Escape
