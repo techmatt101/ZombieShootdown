6119157bc1dfe6a116b1656d793059805301c4cb
techmatt101 <matt@kempmail.co.uk>
2014-11-26 02:37:41 +0000

    added support of IE pixel smoothing

--- src/core/render/PixelFilter.ts
+++ src/core/render/PixelFilter.ts
@@ -1,6 +1,7 @@
 class PixelFilter implements IFilter {
     init (ctx : CanvasRenderingContext2D, canvas : Canvas, camera : Camera) {
         canvas.context.imageSmoothingEnabled = false;
+        canvas.context.msImageSmoothingEnabled = false;
         canvas.context.mozImageSmoothingEnabled = false;
         canvas.context.oImageSmoothingEnabled = false;
         canvas.context.webkitImageSmoothingEnabled = false;
@@ -8,6 +9,7 @@ class PixelFilter implements IFilter {
 
     close (ctx : CanvasRenderingContext2D, canvas : Canvas, camera : Camera) {
         canvas.context.imageSmoothingEnabled = true;
+        canvas.context.msImageSmoothingEnabled  = true;
         canvas.context.mozImageSmoothingEnabled = true;
         canvas.context.oImageSmoothingEnabled = true;
         canvas.context.webkitImageSmoothingEnabled = true;
